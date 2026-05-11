2b4558d743ebfcf4210be6e120594927836da17c
Kleshzz <vasyapulopapik@gmail.com>
2026-05-11 15:09:38 +0300

    Fix probe and init function call logic

--- src/kernel/driver_manager/driver_manager.c
+++ src/kernel/driver_manager/driver_manager.c
@@ -22,8 +22,9 @@ int drvman_register_driver(struct driver_info* driver, struct dev_info* device){
 
     int (*probe)(struct dev_info*) = (int (*)(struct dev_info*))(driver->probe);
     int (*init)(struct dev_info*) = (int (*)(struct dev_info*))(driver->init);
+    int probe_result = 0;
     if (probe != 0) probe_result = probe(device);
-    else probe_result = init(device);
+    else probe_result = init(device);;
 
     return DRIVER_COUNT-1;
 }
